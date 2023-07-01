ce9a544b02bc580d07acc477e397dcf9e892abaf
adam <adam.frenzen@gmail.com>
2023-06-30 22:53:21 -0600

    Angular Portfolio App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { BioComponent } from './bio/bio.component';
+import { SkillsComponent } from './skills/skills.component';
+import {NgOptimizedImage} from "@angular/common";
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { HeaderComponent } from './header/header.component'
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BioComponent,
+    SkillsComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    NgOptimizedImage,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
